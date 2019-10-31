81e7264da6fddaef25cfb2c2bcf957726927e75c
Guo-shiqi <1578296899@qq.com>
2019-10-31 16:00:54 +0800

    简单的数据库和后台

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 var Main = {
-    data() {
-      return {
-        form: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-        }
-      }
-    },
-    methods: {
-      onSubmit() {
-        console.log('submit!');
+  data() {
+    return {
+      form: {
+        name: '',
+        ID: ''
       }
     }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!');
+      this.$http.post('./post.php', { name:this.form.name, id:this.form.ID }, { emulateJSON: true }).then(function (res) {
+        document.write(res.body);
+      }, function (res) {
+        console.log(res.status);
+      });
+    }
   }
+}
 var Ctor = Vue.extend(Main)
 new Ctor().$mount('#app')
