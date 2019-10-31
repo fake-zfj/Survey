f553785e78b64f19d24e39da7fc692c84b179517
Guo-shiqi <1578296899@qq.com>
2019-10-31 17:31:24 +0800

    表单有效性校验

--- main.js
+++ main.js
@@ -1,19 +1,60 @@
 var Main = {
   data() {
+    var check_id = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('学号不可以为空'));
+      } else {
+        setTimeout(() => {
+          if (!Number.isInteger(value)) {
+            callback(new Error('请输入数字值'));
+          } else {
+            if (value <= 100000000 || value >= 200000000) {
+              callback(new Error('学号格式错误'));
+            } else {
+              callback();
+            }
+          }
+        }, 1000);
+      }
+    }
+    var check_name = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('姓名不可以为空'));
+      } else {
+        callback();
+      }
+    }
+
+
     return {
       form: {
         name: '',
-        ID: ''
+        ID: undefined
+      },
+      rules: {
+        name: [
+          { validator: check_name, trigger: 'blur' }
+        ],
+        ID: [
+          { validator: check_id, trigger: 'blur' }
+        ]
       }
     }
   },
   methods: {
-    onSubmit() {
-      console.log('submit!');
-      this.$http.post('./post.php', { name:this.form.name, id:this.form.ID }, { emulateJSON: true }).then(function (res) {
-        document.write(res.body);
-      }, function (res) {
-        console.log(res.status);
+    onSubmit(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+          this.$http.post('./post.php', { name: this.form.name, id: this.form.ID }, { emulateJSON: true }).then(function (res) {
+            document.write(res.body);
+          }, function (res) {
+            alert(res.status);
+          });
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
       });
     }
   }
