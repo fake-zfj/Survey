fa4bed52004c33ff78ae5722d59135e7a8613402
Guo-shiqi <1578296899@qq.com>
2019-11-16 23:13:48 +0800

    更改了数据收集项

--- post.php
+++ post.php
@@ -11,7 +11,8 @@ if(! $conn )
 mysqli_query($conn , "set names utf8");
 $name=$_POST['name'];
 $id=$_POST['id'];
-$sql = "INSERT INTO `survey` (`name`,`id`) VALUES('$name',$id);";
+$add=$_POST['address'];
+$sql = "INSERT INTO `survey` (`name`,`id`,`address`) VALUES('$name',$id,'$add');";
 mysqli_select_db( $conn, 'test' );
 $retv = mysqli_query( $conn, $sql );
 if(! $retv )
